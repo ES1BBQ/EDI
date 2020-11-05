43811bafa5abd560d31144afbfe69d0f27d26ff2
Jaan Janesmae <git@janesmae.com>
2020-11-05 22:15:07 +0200

    Update EDI generator method, add locator info to log in nato fonetic

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
 'use strict';
 (function () {
-    /** Method to update the generated EDI log */
-    const updateEDI = function () {
-        var finalEDI = "[REG1TEST;1]\n" +
-            "TName=ULL kv " + document.getElementById('PBand').value + "\n" +
-            "TDate=" + document.getElementById('TDate').value.replaceAll('-', '') + ";" + document.getElementById('TDate').value.replaceAll('-', '') + "\n" +
-            "PCall=" + document.getElementById('PCall').value.toUpperCase() + "\n" +
-            "PWWLo=" + document.getElementById('PWWLo').value.toUpperCase() + "\n" +
-            "PSect=" + document.getElementById('PSect').value + "\n" +
-            "PBand=" + document.getElementById('PBand').value + "\n" +
-            "RName=" + document.getElementById('RName').value + "\n" +
-            "RCall=" + document.getElementById('PCall').value.toUpperCase() + "\n" +
-            "RCoun=" + document.getElementById('RCoun').value + "\n" +
-            "MOpe1=" + document.getElementById('PCall').value.toUpperCase() + "\n" +
-            "STXEq=" + document.getElementById('STXEq').value + "\n" +
-            "SPowe=" + document.getElementById('SPowe').value + "\n" +
-            "SRXEq=" + document.getElementById('SRXEq').value + "\n" +
-            "SAnte=" + document.getElementById('SAnte').value + "\n" +
-            "SAntH=" + document.getElementById('SAntH').value + "\n" +
-            "[Remarks]\n" +
-            "[QSORecords;1]\n" +
-            "201103;1800;ES1BBQ;6;59;;59;;;KO29IK;0;;N;N;";
-        document.getElementById('finalEDI').value = finalEDI;
-    };
 
     /** Method to generate tabs **/
     const tabify = function (e) {
@@ -50,6 +27,74 @@
         default_tab_index = default_tab_index === -1 ? 0 : default_tab_index;
         setTab(default_tab_index);
         tab_headers.forEach((x, i) => x.onclick = event => setTab(i));
+    };
+
+    /** Turn locator to nato fonetic */
+    const ch2nato = function(ch) {
+        switch (ch.toLowerCase()) {
+            case "a": return "ALPHA";
+            case "b": return "BRAVO";
+            case "c": return "CHARLIE";
+            case "d": return "DELTA";
+            case "e": return "ECHO";
+            case "f": return "FOXTROT";
+            case "g": return "GOLF";
+            case "h": return "HOTEL";
+            case "i": return "INDIA";
+            case "j": return "JULIET";
+            case "k": return "KILO";
+            case "l": return "LIMA";
+            case "m": return "MIKE";
+            case "n": return "NOVEMBER";
+            case "o": return "OSCAR";
+            case "p": return "PAPA";
+            case "q": return "QUEBEC";
+            case "r": return "ROMEO";
+            case "s": return "SIERRA";
+            case "t": return "TANGO";
+            case "u": return "UNIFORM";
+            case "v": return "VICTOR";
+            case "w": return "WHISKEY";
+            case "x": return "RAY";
+            case "y": return "YANKEE";
+            case "z": return "ZULU";
+        }
+        return ch;
+    }
+
+    /** Method to generate/update EDI log */
+    const updateEDI = function () {
+        /** Update locator value */
+        let loc='';
+        for (var i = 0; i < localStorage['PWWLo'].length; i++) {
+            loc = loc.concat( (i==0) ? '' : ' ', ch2nato(localStorage['PWWLo'].toLowerCase().charAt(i)) )
+        }
+        document.getElementById('locator').value = loc;
+
+        const listQSORecords = function(i) {
+            finalEDI = finalEDI.concat("\n201103;1800;ES1BBQ;6;59;;59;;;KO29IK;0;;N;N;");
+        };
+        var finalEDI = "[REG1TEST;1]\n" +
+            "TName=ULL kv " + document.getElementById('PBand').value + "\n" +
+            "TDate=" + localStorage['TDate'].replaceAll('-', '') + ";" + localStorage['TDate'].replaceAll('-', '') + "\n" +
+            "PCall=" + localStorage['PCall'].toUpperCase() + "\n" +
+            "PWWLo=" + localStorage['PWWLo'].toUpperCase() + "\n" +
+            "PSect=" + localStorage['PSect'] + "\n" +
+            "PBand=" + localStorage['PBand'] + "\n" +
+            "RName=" + localStorage['RName'] + "\n" +
+            "RCall=" + localStorage['PCall'].toUpperCase() + "\n" +
+            "RCoun=" + localStorage['RCoun'] + "\n" +
+            "MOpe1=" + localStorage['PCall'].toUpperCase() + "\n" +
+            "STXEq=" + localStorage['STXEq'] + "\n" +
+            "SPowe=" + localStorage['SPowe'] + "\n" +
+            "SRXEq=" + localStorage['SRXEq'] + "\n" +
+            "SAnte=" + localStorage['SAnte'] + "\n" +
+            "SAntH=" + localStorage['SAntH'] + "\n";
+            finalEDI = finalEDI.concat( "[Remarks]\n", (localStorage['remarks'] && localStorage['remarks'].length>1) ? localStorage['remarks'] + "\n" : "" );
+        const QSORecords = JSON.parse(localStorage['QSORecords'] || "[]");
+        finalEDI = finalEDI + "[QSORecords;" + QSORecords.length + "]";
+        QSORecords.forEach(x => listQSORecords(x));
+        document.getElementById('finalEDI').value = finalEDI;
     };
 
     /** Method to store and restore input field values for EDI header */
@@ -83,4 +128,5 @@
     document.querySelectorAll('#edi_header input').forEach(x => inputSaveAndRestore(x));
     document.querySelectorAll('#edi_header select').forEach(x => selectSaveAndRestore(x));
     updateEDI();
+
 })();

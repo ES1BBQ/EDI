1308edfdf8b148d8f43007f05e782a3f25bd30f6
Jaan Jänesmäe <git@janesmae.com>
2021-01-05 21:21:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -174,7 +174,7 @@
         document.getElementById('my_callsign_fonetic').innerHTML = loc;
 
         const listQSORecords = function (i) {
-            finalEDI = finalEDI.concat("\n" + localStorage['TDate'].replace(/-/g, '') + ";" + i[0] + ";" + i[1] + ";" + i[3] + ";" + i[4] + ";;" + i[5] + ";;;" + i[2] + ";0;;N;N;");
+            finalEDI = finalEDI.concat("\n" + localStorage['TDate'].substr(2).replace(/-/g, '') + ";" + i[0] + ";" + i[1] + ";" + i[3] + ";" + i[4] + ";;" + i[5] + ";;;" + i[2] + ";0;;N;N;");
         };
         var finalEDI = "[REG1TEST;1]\n";
         finalEDI = finalEDI.concat("TName=ULL kv ", (localStorage['PBand']) ? localStorage['PBand'] : document.getElementById('PBand').value, "\n");
